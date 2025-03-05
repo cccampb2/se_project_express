02695b1c2da19e86b48c6b548e9130f61c0aa342
cccampb2 <calebccampbell19@gmail.com>
2025-03-05 00:01:21 -0500

    feat(routes/index.js) add signIn/signUp routes

--- models/users.js
+++ models/users.js
@@ -50,7 +50,7 @@ userSchema.statics.findUserByCredentials = function findUserByCredentials(
         return Promise.reject(new Error("Incorrect email or password"));
       }
 
-      return user; // now user is available
+      return user;
     });
   });
 };
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const { NOT_FOUND } = require("../utils/errors");
 
 const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItems");
+const { login, createUser } = require("../controllers/users");
 
-router.use("/users", userRouter);
 router.use("/items", clothingItemRouter);
+router.post("/signin", login);
+router.post("/signup", createUser);
 
 router.use((req, res) => {
   res.status(NOT_FOUND).json({ message: "Resource not found" });
